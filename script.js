2754897a95729bbfc8afefa08a57a03a6e43521f
Allen Fordham <allen.s.fordham@gmail.com>
2016-12-06 11:04:08 -0700

    last push may have increased bugs... tweaking

--- script.js
+++ script.js
@@ -151,7 +151,7 @@ function reload_img () {
 }
 
 function error_img () {
-  setTimeout("mjpeg_img.src = 'cam_pic.php?time=' + new Date().getTime();", 100);
+  setTimeout("mjpeg_img.src = PiUrl + 'cam_pic.php?time=' + new Date().getTime();", 100);
 }
 
 function updatePreview(cycle)
@@ -169,7 +169,7 @@ function updatePreview(cycle)
       {
          if(!halted)
          {
-            mjpeg_img.src = "cam_pic_new.php?time=" + new Date().getTime() + "&pDelay=" + preview_delay;
+            mjpeg_img.src = PiUrl + "cam_pic_new.php?time=" + new Date().getTime() + "&pDelay=" + preview_delay;
          }
          else
          {
